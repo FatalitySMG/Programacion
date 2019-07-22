af974473e525a7abb51dd0ef6a451849606f38c0
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-07-22 12:46:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            //Console.WriteLine("Solucion 1");
+            //double y = 2;
+            //double z = 3;
+            //double pi = 3.14159265;
+
+            //double t = Math.Sqrt((y * y) + (z * z));
+            //double c = (Math.Atan(z / y) * (180/pi));
+            //double a = (Math.Atan(y / z) * (180 / pi));
+
+            //Console.WriteLine("t =" + t);
+            //Console.WriteLine("c =" + c);
+            //Console.WriteLine("a =" + a);
+
+            //Ejercicio 2
+            //Console.WriteLine("Solucion 2");
+            //double t = 3.61;
+            //double a = 33.69;
+            //double pi = 3.14159265;
+
+            //double y = t * Math.Sin(a * pi/180);
+            //double z = Math.Sqrt((t * t) - (y * y));
+            //double c = 180 - (90 + a);
+
+            //Console.WriteLine("Y =" + y);
+            //Console.WriteLine("c =" + c);
+            //Console.WriteLine("z =" + z);
+
+            //Ejercicio 3
+            //Console.WriteLine("Solucion 3");
+            //double c = 56.31;
+            //double z = 3;
+            //double pi = 3.14159265;
+
+            //double y = z / Math.Tan(c * pi/180);
+            //double t = z / Math.Sin(c * pi/180);
+            //double a = 180 - (90 + c);
+
+            //Console.WriteLine("Y =" + y);
+            //Console.WriteLine("t =" + t);
+            //Console.WriteLine("a =" + a);
+
+        }
+    }
+}
