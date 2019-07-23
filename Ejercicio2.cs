2c3a52bebda78a503d809380578e4a0efb5bcfc3
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-07-23 17:41:22 -0500

    Add files via upload

---
+++ Ejercicio2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            //Console.WriteLine("Valor de x");
+            //double b = 19.44; 
+            //double pi = 3.14159265;
+            //double z = 3;
+            //double y = 2;
+
+            //double c = Math.Atan(z / y) * 180/pi; //bueno
+            //double a = Math.Atan(y / z) * 180/pi;  //bueno
+            //double d = 180 - (90 + a + b);  //bueno
+            //double w =  z / Math.Sin(d * pi/180); // bueno
+            //double x = ((Math.Sin((b + a) * pi/180) ) * w) -y; // bueno
+
+            //Console.WriteLine("x = " + x);
+
+            //Ejercicio 2
+            //Console.WriteLine("Valor de z");
+            //double d = 36.87;
+            //double b = 19.44;
+            //double y = 2;
+            //double pi = 3.14159265;
+
+            //double a = 180 - (90 + d + b); // bueno
+            //double c = 180 - (90 + a);  // bueno
+            //double t = y / Math.Sin(a * pi / 180) ; // bueno
+            //double z = t * Math.Sin(c * pi / 180); // bueno
+
+            //Console.WriteLine("z = " + z);
+
+            //Ejercicio 3
+            //Console.WriteLine("Valor de y");
+            //double w = 5;
+            //double d = 36.87;
+            //double x = 2;
+            //double pi = 3.14159265;
+
+            //double z = 5 * Math.Sin(d * pi / 180) / Math.Sin(90 * pi / 180); //bueno
+            ////Por propiedades matematicas sabemos que un triangulo con hipotenusa 5 y un cateto del valor 3 o 4 cumple las normas de el tiangulo pitagorico el cual cumple con las reglas de el teorema de pitagoras por lo tanto 
+            //double y = Math.Sqrt((w * w) - (z * z)) - x; // bueno
+            //Console.WriteLine("y = " + y);
+
+            //Ejercicio 4
+            //Console.WriteLine("Valores de x");
+            //double w = 5;
+            //double t = 3.61;
+            //double c = 56.31;
+            //double pi = 3.14159265;
+
+            //double z = t * Math.Sin(c * pi / 180);
+            //double y = Math.Sqrt((t * t) - (z * z));
+            ////Por propiedades matematicas sabemos que un triangulo con hipotenusa 5 y un cateto del valor 3 o 4 cumple las normas de el tiangulo pitagorico el cual cumple con las reglas de el teorema de pitagoras por lo tanto 
+            //double x = Math.Sqrt((w * w) - (z * z)) - y; // bueno
+            //Console.WriteLine("x = " + x);
+
+
+
+
+
+
+        }
+    }
+}
