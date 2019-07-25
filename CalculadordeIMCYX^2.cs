f86e77384a0c50e6f62f7f09dd8121685d0008ba
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-07-25 09:39:01 -0500

    Add files via upload

---
+++ CalculadordeIMCYX^2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_clase
+{
+    class GeneradorIMC
+    {
+        static void Main()
+        {
+             //Calculadora de IMC
+
+            /*Console.WriteLine("Descubre tu indice de masa corporal IMC ");
+
+            Console.WriteLine("Ingrese su peso en kg");
+            int peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura en m (para esto use , en vez de .");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            if (imc < 20)
+            {
+                Console.WriteLine("Peso bajo");
+            }
+            else if (20 <= imc && imc <= 23.9)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if(24 <= imc && imc <= 28.9)
+            {
+                Console.WriteLine("Leve sobrepeso");
+            }
+            else if (29 <= imc && imc <= 37)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            if (imc > 40)
+            {
+                Console.WriteLine("Obesidad severa");
+            }
+        }*/
+               //Calculadora de ecuaciones cuadraticas
+
+            /*Console.WriteLine("calcule los valores de ecuaciones de la forma Ax^2+Bx+C=0");
+
+            Console.WriteLine("Ingrese el valor de A");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de B");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de C");
+            double c = double.Parse(Console.ReadLine());
+
+            double z = Math.Pow(b,2) - (4 * a * c);
+
+            double x1 = (-b + Math.Sqrt(z)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(z)) / (2 * a);
+
+            if (z < 0)
+            {
+                Console.WriteLine("Error");
+            }
+            if (z == 0)
+            {
+                Console.WriteLine("Una sola solucion");
+                Console.WriteLine("X1 =" + x1);
+            }
+            if (z > 0)
+            {
+                Console.WriteLine("Dos soluciones");
+                Console.WriteLine("X1 =" + x1);
+                Console.WriteLine("X2 =" + x2);
+            }*/
+
+
+        }
+    }
+}
+         
+    
+
