29928f64865c8b73564e4f845586b39c749a98e2
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-07-25 12:48:44 -0500

    Add files via upload

---
+++ Desafio2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafios_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Revisado* //Caja de compensacion 
+
+            /* Console.WriteLine("Tarifa de Gimnasio El Viga");
+
+             Console.WriteLine("Ingrese el valor total de su sueldo");
+             double smmlv = 828116;
+             double t1 = 30000;
+             double t2 = 50000;
+             double t3 = 70000;
+
+
+             double x = double.Parse(Console.ReadLine());
+
+             if (x < (2* smmlv))
+             {
+                 Console.WriteLine("Tarifa =" + t1);
+             }
+             else if ( (2*smmlv) <= x && x <= (4*smmlv))
+             {
+                 Console.WriteLine("Tarifa =" + t2);
+             }
+             else if (x > (4 * smmlv))
+             {
+                 Console.WriteLine("Tarifa =" + t3);
+             }*/
+
+            //Revisado* //Tarifas de EPS
+            // Pagina d ela que obtuve la informacion https://pospopuli.minsalud.gov.co/PospopuliWeb/files/Cuotasmoderadorasycopagos2019.pdf
+
+           /* Console.WriteLine("Valor cuota moderadora para citas generales de la EPS");
+            Console.WriteLine("Ingrese valor total de su sueldo");
+            double smmlv = 828116;
+            double t1 = 3000;
+            double t2 = 12000;
+            double t3 = 31600;
+
+
+            double x = double.Parse(Console.ReadLine());
+
+            if (x < (2 * smmlv))
+            {
+                Console.WriteLine("Tarifa =" + t1);
+            }
+            else if ((2 * smmlv) <= x && x <= (5 * smmlv))
+            {
+                Console.WriteLine("Tarifa =" + t2);
+            }
+            else if (x > (5 * smmlv))
+            {
+                Console.WriteLine("Tarifa =" + t3);
+            }*/
+
+        }
+    }
+}
