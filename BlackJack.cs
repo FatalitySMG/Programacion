7396eaba36215adfd044595a280501eadf2ad82c
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-08-08 09:47:49 -0500

    Add files via upload

---
+++ BlackJack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bienvenido a BlackJack: ");
+
+            Random aleatorio = new Random();
+            int carta1 = 0, carta2 = 0, total = 0;
+            int sumaCartas = carta1 + carta2;
+            string continuar = "s";
+
+            while(continuar == "s" && total < 21)
+            {
+                carta1 = aleatorio.Next(1, 11);
+                Console.WriteLine("Carta dada: " + carta1);
+                total += carta1;
+                if (total < 21)
+                {
+                    
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("¿Desea otra Carta? (s/n) ");
+                    continuar = Console.ReadLine();
+                }
+                else Console.WriteLine("Eliminado");
+            }
+        }
+    }
+}
