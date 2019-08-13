fddf70038f5d14f91a8e17aeb0da7447281a1ca9
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-08-13 08:00:24 -0500

    Update BlackJack.cs

--- BlackJack.cs
+++ BlackJack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,22 +16,26 @@ namespace BlackJack
             int carta1 = 0, carta2 = 0, total = 0, sumaCartas = carta1 + carta2;
             string continuar = "s";
 
-            while(continuar == "s" && total < 21)
+            while (continuar == "s" && total < 21)
             {
                 carta1 = aleatorio.Next(1, 11);
                 Console.WriteLine("Carta dada: " + carta1);
                 total += carta1;
                 if (total < 21)
-                {  
+                {
                     Console.WriteLine("Total: " + total);
                     Console.WriteLine("¿Desea otra Carta? (s/n) ");
                     continuar = Console.ReadLine();
                 }
-                
+                else if (total == 21)
+                {
+                    Console.WriteLine("Ganaste");
+                    Console.WriteLine("Total: " + total);
+                }
                 else
                 {
-                  Console.WriteLine("Eliminado");
-                  Console.WriteLine(“Total: “ + total);
+                    Console.WriteLine("Eliminado");
+                    Console.WriteLine("Total: " +total);
                 }
             }
         }
