4be3dccea1ef97de9e9c5efe5b7a8bd638204d39
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-08-22 05:22:57 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace BlackJack
 
             Random aleatorio = new Random();
             int carta1 = 0, carta2 = 0, total = 0, sumaCartas = carta1 + carta2, jugador = 0, n = 5, m = 2, contadorJ = 5;
-            string continuar = "s";
+            string continuar = "s", ganador = "Nadie";
 
             Console.WriteLine("Ingrese el numero de jugadores (minimo 2 maximo 5)");
             jugador = int.Parse(Console.ReadLine());
@@ -29,7 +29,9 @@ namespace BlackJack
             {
                 Console.WriteLine("Inicio del juego");
 
-                Console.WriteLine("\nBienvenido jugador");
+                Console.WriteLine("\nBienvenido jugador: ");
+                Console.WriteLine("Ingrese el nombre del jugador");
+                string nombre = Console.ReadLine();
 
                 while (continuar == "s" && total < 21)
                 {
@@ -45,18 +47,21 @@ namespace BlackJack
                     }
                     else if (total == 21)
                     {
+                        ganador = nombre;
                         Console.WriteLine("Ganaste");
                         Console.WriteLine("Total: " + total);
                         break;
                     }
                     else
                     {
+                        ganador = nombre;
                         Console.WriteLine("Eliminado");
                         Console.WriteLine("Total: " + total);
                         total = 0;
                         break;
                     }
                 }
+                Console.WriteLine("Gandaor: " + ganador);
                 Console.WriteLine("Gracias por participar ");
                 jugador += 1;
 
