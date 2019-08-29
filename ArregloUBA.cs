03a232a743f62092104580ccf6b8160eb9b10a12
FatalitySMG <53127754+FatalitySMG@users.noreply.github.com>
2019-08-29 09:54:36 -0500

    Create ArregloUBA.cs

---
+++ ArregloUBA.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloAlgorBurbuja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 0, 3, 6, 6, 6, 12, 12, 12, 9, 15, 18, 18 };
+            int[] b = { 0, 0, 2, 2, 4, 6, 6, 6, 8, 10, 12, 12, 14, 16, 18, 18 };
+            int[] U = new int[31];
+
+            for(int i= 0; ;)
+            {
+
+            }
+        }
+    }
+}
